fc46b4b7ee5802743dd8c92d3e6b99759807e64d
Dmitri Stadnik <dmitri.stadnik@gmail.com>
2016-05-15 19:07:59 +0300

    order edit menu WIP

--- war/j_workspace.js
+++ war/j_workspace.js
@@ -10,6 +10,7 @@
 		if(authSesTO - dateObj < 0)
 			window.location = "auth.html";
 	
+	var ordersList = {};
 	var tokenJson = {
 			token: authSesToken,
 		};
@@ -23,6 +24,7 @@
 		},
 		
 	});	
+	var orderList;
 	$.ajax({
 		type: 'GET',
 		url: 'https://beta-dot-eduportal-1277.appspot.com/_ah/api/order/v1/allOrders',
@@ -30,20 +32,19 @@
 		success: function(resData) { 
 			for (var i = 0; i < resData.items.length; i++)
 			{
-				$('#table-orders').append("<tr>" +
+				$('#table-orders').append("<tr class='tr-order' id='tr-order-" + (i + 1) + "' >" +
 						"<td>" + (i + 1) + "</td>" +
 						"<td>" + resData.items[i].id + "</td>" +
-						"<td>" + resData.items[i].user.name + " " + resData.items[i].user.surname + "</td>" +
-						"<td>" + resData.items[i].product.title + "</td>" +
-						"<td>" + resData.items[i].createdBy.name + " " + resData.items[i].createdBy.surname + "</td>" +
+						"<td>" + resData.items[i].clientName + "</td>" +
+						"<td>" + resData.items[i].productName + "</td>" +
+						"<td>" + resData.items[i].creatorName + "</td>" +
 						"<td>" + checkBool(resData.items[i].donePaid) + "</td>" +
 					"</tr>");	
 			}
 			$('#li-open-orders').append(" " + i)
 			
 		},
-	});			
-	
+	});	
 		
 	$('#menu-showses').on('click', function(){
 		$resDiv.append("TokenId = " + getCookie("sesToken") + "; tokenTO = " + getCookie("sesTO"));	
@@ -55,6 +56,25 @@
 		window.location = "auth.html";
 	});
 	
+	$('#opacity').on('click', function(){
+		$(this).css('display', 'none');	
+		$('#order-edit-form').css('display', 'none');
+	});
+	
+	$('#table-orders').on("click", "tr", function() {
+		var rowIndex = parseInt( $(this).index() ) - 1;
+		$.ajax({
+		type: 'GET',
+		url: 'https://beta-dot-eduportal-1277.appspot.com/_ah/api/order/v1/allOrders',
+		data: tokenJson,
+		success: function(resData) { 
+			$('label[for="order-id"]').html("ID заказа: " + resData.items[rowIndex].id);	
+			$('#opacity').css('display', 'block');
+			$('#order-edit-form').css('display', 'block');
+		},
+		});	
+	});
+	
 });
 
 function checkBool(data)
@@ -62,4 +82,5 @@ function checkBool(data)
 	if (data = true)
 		return "Да";
 	else return "Нет";
-}
+};
+
