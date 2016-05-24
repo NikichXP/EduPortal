25f308c5d11886ad715d7ffbec7d2c632cb4a9be
Dmitri Stadnik <dmitri.stadnik@gmail.com>
2016-05-25 02:32:17 +0300

    user creation working
    yay

--- war/j_workspace.js
+++ war/j_workspace.js
@@ -77,12 +77,22 @@
 	//hide order edit block
 	$('#opacity').on('click', function(){
 		$(this).css('display', 'none');	
+		
 		$('#order-edit-form').css('display', 'none');
+		
 		$('#order-edit-payment').css('display', 'none');
+		
 		$('#client-list').css('display', 'none');
+		
 		$('#product-list').css('display', 'none');
+		
 		$('#lists-container').css('display', 'none');
+		
 		$('#order-create').css('display', 'none');
+		
+		$('#client-menu').css('display', 'none');
+		$('#client-menu').css('height', '250px');
+		$('#client-menu-create').html("Добавить клиента");
 	});
 	//hide lists block
 	$('#lists-container').on('click', function(){
@@ -144,6 +154,7 @@
 		},
 		});	
 	});
+	
 	//show products table
 	$('#order-product-name').on('click', function(){
 		$('#table-product-list').html("<tbody></tbody>");
@@ -178,6 +189,7 @@
 		},
 		});	
 	});
+	
 	//open payment edit menu
 	$('#table-orders').on("click", "tr.tr-order", function() {
 		
@@ -284,6 +296,86 @@
 			success: location.reload(),	
 			});	
 		};		
+	});
+	
+	//open client menu
+	$('#menu-client-open').on("click", function() {
+				
+		$('#table-client-menu').html("<tbody></tbody>");
+		$('#client-menu-text-block').html("<H2>Мои клиенты</H2>");
+		$('#client-new').css('display', 'none');
+		
+		$.ajax({
+		type: 'GET',
+		url: 'https://beta-dot-eduportal-1277.appspot.com/_ah/api/user/v1/getMyClients',
+		data: tokenJson,
+		success: function(resData) {
+			var imax = resData.items.length;
+			var count = 0;
+			for (var i = 0; i < resData.items.length; i++)
+			{
+				if (count == imax) break;
+				$('#table-client-menu tbody').append("<tr id='tr-client-menu-" + (i + 1) + "'>");
+				for (var j = 0; j < 5; j++)
+				{
+					$('#tr-client-menu-' + (i + 1)).append(
+					"<td class='td-client-menu'>" + resData.items[count].name + " " + resData.items[count].surname + 
+					"<input type='hidden' class='inner-client-menu-input-1' value=" + resData.items[count].id + ">" +
+					"</td>"
+					);
+					count++;
+					if (count == imax) break;
+				}
+				$('#table-client-menu').append("</tr>");				
+			}
+			$('#client-menu').css('display', 'block');
+			$('#opacity').css('display', 'block');
+		},
+		});	
+	});
+	
+	//client creation menu
+	$('#client-menu-create').on('click', function() {
+		
+		$('#client-menu-create').html("Принять");
+		
+		if ($('#client-new').css('display') == 'block')
+		{
+			$('#client-new').css('display', 'none');
+			$('#client-menu-create').html("Добавить клиента");
+			$('#client-menu').css('height', '250px');
+			
+			var clData = new FormData();    
+			clData.append('token', authSesToken);
+			clData.append('mail', $('#client-new-email').val());
+			clData.append('name', $('#client-new-name').val());
+			clData.append('surname', $('#client-new-surname').val());
+			clData.append('phone', $('#client-new-phone').val());
+			clData.append('pass', '');
+
+			/* var clientData = {
+				token: authSesToken,
+				mail: $('#client-new-email').val(),
+				name: $('#client-new-name').val(),
+				surname: $('#client-new-surname').val(),
+				phone: $('#client-new-phone').val(),
+			}; */
+			
+			$.ajax({
+			type: 'POST',
+			url: 'https://beta-dot-eduportal-1277.appspot.com/_ah/api/user/v1/createuser',
+			data: clData,
+			processData: false,
+			contentType: false,
+			//success: location.reload(),	
+			});	
+		}
+		else 
+		{
+			$('#client-new').css('display', 'block');
+			$('#client-menu').css('height', '600px');
+		}
+		
 	});
 });
 
